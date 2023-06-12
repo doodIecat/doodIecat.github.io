9f23e79d73d23a2c713e01f705c8395ca47d6d6c
Inkling <inkling@doodle.cat>
2023-06-12 21:23:31 +0100

    Update index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-// Variable to keep track of mouse movement
-var mouseMoved = false;
+// Create the game canvas
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-// Function to show the mouse cursor
-function showMouseCursor() {
-  document.body.style.cursor = 'auto';
+// Set the canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Define game variables
+let playerX = 50; // Initial player X position
+let playerY = canvas.height / 2; // Initial player Y position
+let gravity = 1; // Gravity value
+let velocity = 0; // Player velocity
+
+// Update player position
+function updatePlayer() {
+    // Apply gravity
+    velocity += gravity;
+    playerY += velocity;
+
+    // Stop player from falling through the bottom
+    if (playerY > canvas.height) {
+        playerY = canvas.height;
+        velocity = 0;
+    }
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the player
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(playerX, playerY, 50, 50);
+
+    // Call the updatePlayer function again
+    requestAnimationFrame(updatePlayer);
 }
 
-// Function to hide the mouse cursor
-function hideMouseCursor() {
-  document.body.style.cursor = 'none';
+// Handle key events
+function handleKeyDown(event) {
+    if (event.code === "Space") {
+        // Jump when the space key is pressed
+        velocity -= 15;
+    }
 }
 
-// Event listener for key press
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'h') {
-    hideMouseCursor();
-  }
-});
+// Add event listener for keydown events
+document.addEventListener("keydown", handleKeyDown);
 
-// Event listener for mouse movement
-document.addEventListener('mousemove', function() {
-  if (!mouseMoved) {
-    showMouseCursor();
-    mouseMoved = true;
-  }
-});
+// Start the game loop
+requestAnimationFrame(updatePlayer);
