6522ee1b3c1a8e0a91d3a3b309727c8fe72b5338
Inkling <inkling@doodle.cat>
2023-06-14 12:00:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-const sentences = [
-    "I must not hold a bring your pet rock to school day",
-    "I will not enroll the school hampster in a Tai Chi class",
-    "I must not organize a field trip to the moon",
-    "I must not convince my classmates I am a time traveler",
-    "I will not change the school fire alarm to a recording of my laughter",
-    "I will not squash the principles car then blame it on aliens",
-    "I must not wear a hat taller than the school on crazy hat day",
-    "I will not replace all the desks with trampolines",
-    "I must not implement policies such as mandatory pizza for lunch",
-    "I must not sit on a beanbag during class then take random naps",
-    "I will not setup a bouncy castle during chemistry",
-    "I must not turn the school cafeteria into a disco",
-    "I will not replace all the textbooks with coloring books",
-    "I must not teach squirrels calculus",
-    "I will not place a cactus on the teachers chair",
-    "I will not declare the school libary a no read zone",
-    "I must not replace the water fountain with a chocolate milk dispenser",
-    "I will not replace all textbooks with comics",
-    "I will not replace all pencils with giant lollipops",
-    "I must not organize a whistling performance during assembly",
-  ];
+let language = "catalan"
+
+if(language = "english") {
+    const sentences = [
+        "I must not hold a bring your pet rock to school day",
+        "I will not enroll the school hampster in a Tai Chi class",
+        "I must not organize a field trip to the moon",
+        "I must not convince my classmates I am a time traveler",
+        "I will not change the school fire alarm to a recording of my laughter",
+        "I will not squash the principles car then blame it on aliens",
+        "I must not wear a hat taller than the school on crazy hat day",
+        "I will not replace all the desks with trampolines",
+        "I must not implement policies such as mandatory pizza for lunch",
+        "I must not sit on a beanbag during class then take random naps",
+        "I will not setup a bouncy castle during chemistry",
+        "I must not turn the school cafeteria into a disco",
+        "I will not replace all the textbooks with coloring books",
+        "I must not teach squirrels calculus",
+        "I will not place a cactus on the teachers chair",
+        "I will not declare the school libary a no read zone",
+        "I must not replace the water fountain with a chocolate milk dispenser",
+        "I will not replace all textbooks with comics",
+        "I will not replace all pencils with giant lollipops",
+        "I must not organize a whistling performance during assembly",
+      ];
+} else if(language = "catalan") {
+    const sentences = [
+        "No he d'agafar una penya per portar la teva mascota a l'escola",
+        "No matricularé l'hamster de l'escola en una classe de Tai-txi",
+        "No he d'organitzar una excursió a la lluna",
+        "No he de convèncer els meus companys que sóc un viatger en el temps",
+        "No canviaré l'alarma d'incendi de l'escola per una gravació del meu riure",
+        "No aixafaré els principis perquè després culparé als extraterrestres",
+        "I must not wear a hat taller than the school on crazy hat day",
+        "I will not replace all the desks with trampolines",
+        "I must not implement policies such as mandatory pizza for lunch",
+        "I must not sit on a beanbag during class then take random naps",
+        "I will not setup a bouncy castle during chemistry",
+        "I must not turn the school cafeteria into a disco",
+        "I will not replace all the textbooks with coloring books",
+        "I must not teach squirrels calculus",
+        "I will not place a cactus on the teachers chair",
+        "I will not declare the school libary a no read zone",
+        "I must not replace the water fountain with a chocolate milk dispenser",
+        "I will not replace all textbooks with comics",
+        "I will not replace all pencils with giant lollipops",
+        "I must not organize a whistling performance during assembly",
+      ];
+}
   
   let currentSentence = "";
   
