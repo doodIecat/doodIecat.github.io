ae6b361ca0582bbb70ea69c2f3466ac5fe72ad47
Inkling <inkling@doodle.cat>
2023-06-14 12:13:07 +0100

    update

--- script.js
+++ script.js
@@ -48,90 +48,87 @@ if(language == "english") {
       ];
 }
   
-  let currentSentence = "";
-  
-  function generateRandomSentence() {
-    const randomIndex = Math.floor(Math.random() * sentences.length);
-    currentSentence = sentences[randomIndex].toUpperCase();
-  
-    const textDiv = document.getElementById("text");
-    textDiv.innerHTML = ""; // Clear previous content
-  
-    for (let i = 0; i < currentSentence.length; i++) {
-      const charSpan = document.createElement("span");
-      charSpan.textContent = currentSentence[i];
-      charSpan.style.fontSize = "36px"; // Increase the text size to 36 pixels
-      textDiv.appendChild(charSpan);
-    }
+let currentSentence = "";
+
+function generateRandomSentence() {
+  const randomIndex = Math.floor(Math.random() * sentences.length);
+  currentSentence = sentences[randomIndex].toUpperCase();
+
+  const textDiv = document.getElementById("text");
+  textDiv.innerHTML = ""; // Clear previous content
+
+  for (let i = 0; i < currentSentence.length; i++) {
+    const charSpan = document.createElement("span");
+    charSpan.textContent = currentSentence[i];
+    charSpan.style.fontSize = "36px"; // Increase the text size to 36 pixels
+    textDiv.appendChild(charSpan);
   }
-  
-  function handleKeyPress(event) {
-    const inputText = event.target.value.toLowerCase();
-    const textDiv = document.getElementById("text");
-    const charSpans = textDiv.children;
-  
-    for (let i = 0; i < charSpans.length; i++) {
-      const charSpan = charSpans[i];
-  
-      if (i < inputText.length) {
-        if (inputText[i] === currentSentence[i].toLowerCase()) {
-          charSpan.style.color = "#000000"; // Black
-          charSpan.style.textDecoration = "none";
-          charSpan.style.borderBottom = "2px solid transparent";
-        } else {
-          charSpan.style.color = "#FF0000"; // Red
-          charSpan.style.textDecoration = "none";
-          charSpan.style.borderBottom = "2px solid #FF0000"; // Red
-        }
-      } else {
-        charSpan.style.color = "#D1D1D1"; // Light gray
+}
+
+function handleKeyPress(event) {
+  const inputText = event.target.value.toLowerCase();
+  const textDiv = document.getElementById("text");
+  const charSpans = textDiv.children;
+
+  for (let i = 0; i < charSpans.length; i++) {
+    const charSpan = charSpans[i];
+
+    if (i < inputText.length) {
+      if (inputText[i] === currentSentence[i].toLowerCase()) {
+        charSpan.style.color = "#000000"; // Black
         charSpan.style.textDecoration = "none";
         charSpan.style.borderBottom = "2px solid transparent";
+      } else {
+        charSpan.style.color = "#FF0000"; // Red
+        charSpan.style.textDecoration = "none";
+        charSpan.style.borderBottom = "2px solid #FF0000"; // Red
       }
+    } else {
+      charSpan.style.color = "#D1D1D1"; // Light gray
+      charSpan.style.textDecoration = "none";
+      charSpan.style.borderBottom = "2px solid transparent";
     }
   }
-  
-  generateRandomSentence();
-  
-  const inputElement = document.createElement("input");
-  inputElement.type = "text";
-  inputElement.style.display = "none"; // Hide the input box
-  inputElement.addEventListener("input", handleKeyPress);
-  document.getElementById("container").appendChild(inputElement);
-  
-  let isCtrlKeyPressed = false;
-  
-  document.addEventListener("keydown", function(event) {
-    const key = event.key;
-  
-    if (key === "Backspace") {
-      event.preventDefault();
-      const isCtrlPressed = event.ctrlKey || event.metaKey; // Check if Ctrl key is pressed
-      if (isCtrlPressed) {
-        const words = inputElement.value.trim().split(" ");
-        if (words.length > 1) {
-          words.pop(); // Remove the last word
-          inputElement.value = words.join(" ") + " "; // Add space after removing the word
-        } else {
-          inputElement.value = ""; // Clear the input if only one word is present
-        }
+}
+
+generateRandomSentence();
+
+const inputElement = document.createElement("input");
+inputElement.type = "text";
+inputElement.style.display = "none"; // Hide the input box
+inputElement.addEventListener("input", handleKeyPress);
+document.getElementById("container").appendChild(inputElement);
+
+let isCtrlKeyPressed = false;
+
+document.addEventListener("keydown", function(event) {
+  const key = event.key;
+
+  if (key === "Backspace") {
+    event.preventDefault();
+    const isCtrlPressed = event.ctrlKey || event.metaKey; // Check if Ctrl key is pressed
+    if (isCtrlPressed) {
+      const words = inputElement.value.trim().split(" ");
+      if (words.length > 1) {
+        words.pop(); // Remove the last word
+        inputElement.value = words.join(" ") + " "; // Add space after removing the word
       } else {
-        inputElement.value = inputElement.value.slice(0, -1);
+        inputElement.value = ""; // Clear the input if only one word is present
       }
-      handleKeyPress({ target: inputElement });
-    } else if (/^[a-z\s]$/i.test(key)) {
-      inputElement.value += key;
-      handleKeyPress({ target: inputElement });
-    } else if (key === "Control") {
-      isCtrlKeyPressed = true;
-    } else if (key === "'") {
-      inputElement.value += "'";
+    } else {
+      inputElement.value = inputElement.value.slice(0, -1);
     }
-  });
-  
-  document.addEventListener("keyup", function(event) {
-    if (event.key === "Control") {
-      isCtrlKeyPressed = false;
-    }
-  });
-  
+    handleKeyPress({ target: inputElement });
+  } else if (/^[a-z\s]$/i.test(key)) {
+    inputElement.value += key;
+    handleKeyPress({ target: inputElement });
+  } else if (key === "Control") {
+    isCtrlKeyPressed = true;
+  }
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.key === "Control") {
+    isCtrlKeyPressed = false;
+  }
+});
